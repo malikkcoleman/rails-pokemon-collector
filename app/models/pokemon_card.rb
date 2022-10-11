a3f11370d2bf6ca7ac561b8d994716239e9eb9c1
malikkcoleman <malikcoleman@live.ca>
2022-10-11 11:41:46 -0500

    Adding validations for 1.6

--- app/models/pokemon_card.rb
+++ app/models/pokemon_card.rb
@@ -1,2 +1,5 @@
 class PokemonCard < ApplicationRecord
+  belongs_to :pokemon_sets
+
+  validates :name, :card_number, :card_type, :image, presence:true
 end
--- app/models/series.rb
+++ app/models/series.rb
@@ -1,2 +1,4 @@
 class Series < ApplicationRecord
+
+  validates :name, :image, presence:true
 end
