ba732543e24b2b46f10d63b28462e30a39de8519
malikkcoleman <malikcoleman@live.ca>
2022-10-19 22:51:58 -0500

    made nav bar and all layouts resposnive with flexbox

--- app/javascript/packs/menu.js
+++ app/javascript/packs/menu.js
@@ -1,7 +1,3 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 
